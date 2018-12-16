960a989c034640f874889d150f8d975f3c70bb5d
André Henniger <ahenniger95@gmail.com>
2018-12-16 18:30:12 +0100

    XML Parser funktioniert

--- SecondReiseplaner/src/XMLParser.java
+++ SecondReiseplaner/src/XMLParser.java
@@ -1,25 +1,31 @@
 import java.io.File;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class XMLParser {
 
-	public void parse() {
-		File file = new File("userdata.xml");
+	public static void parse() {
+		File file = new File("features.xml");
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder documentBuilder;
 		try {
 			documentBuilder = documentBuilderFactory.newDocumentBuilder();
-			Document document = documentBuilder.parse(file);
+			Document document = documentBuilder.parse(file); 
+			System.out.println(document.getElementsByTagName("Suchleiste").item(0).getTextContent());
 		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+      
 		//String usr = document.getElementsByTagName("user").item(0).getTextContent();
 		//String pwd = document.getElementsByTagName("password").item(0).getTextContent();
 	}
